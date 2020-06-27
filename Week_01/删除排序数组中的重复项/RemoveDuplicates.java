5048faf505b919ef987822b86cd915752efc03ac
hejianhui <org_hejianhui@163.com>
2020-06-27 18:21:46 +0800

    week_01

---
+++ Week_01/删除排序数组中的重复项/RemoveDuplicates.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * <p>
+ * 26. 删除排序数组中的重复项（Facebook、字节跳动、微软在半年内面试中考过）
+ * </p>
+ *
+ * @author: hejianhui
+ * @create: 2020-06-27 16:25
+ * @see RemoveDuplicates
+ * @since JDK1.8
+ */
+public class RemoveDuplicates {
+
+    public static void main(String[] args) {
+        System.out.println(removeDuplicates(new int[]{1,1,2}));
+    }
+
+    public static int removeDuplicates(int[] nums) {
+        if(nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        int p = 0; // 前指针
+        int q = 1;// 后指针
+
+        while (q < nums.length){
+            if (nums[p] != nums[q]) {
+                nums[p + 1] = nums[q];
+                p++;
+            }
+            q++;
+        }
+        return p + 1;
+
+    }
+
+}
---
+++ Week_01/旋转数组/Rotate.java
@@ -0,0 +1,14 @@
+package 旋转数组;
+
+/**
+ * <p>
+ * 189. 旋转数组
+ * </p>
+ *
+ * @author: hejianhui
+ * @create: 2020-06-27 18:09
+ * @see Rotate
+ * @since JDK1.8
+ */
+public class Rotate {
+}
